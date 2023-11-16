11d7058d298a40cbd2a9e6f22ad0d4f9afd5c900
enriqueGaleanoT <enriquej999@gmail.com>
2023-11-15 21:19:19 -0600

    node promises pruebas con el metodo then()

--- app.js
+++ app.js
@@ -1,3 +1,125 @@
+
+//Pruebas con el metodo Asyn
+// const readline = require('readline-sync');
+
+// // Lista de tareas
+// const tasks = [];
+
+// function addTarea() {
+//     return new Promise((resolve, reject) => {
+//         console.log(' ');
+//         const addTareas = readline.question('Anade una tarea: ');
+//         console.log('Cargando....')
+//         setTimeout(() => {
+//             tasks.push({ addTareas, completado: false });
+//             resolve(addTareas);
+//         }, 3000);
+//     });
+// }
+
+// async function addTareaAsync() {
+//     try {
+//         let miTareaAsync = await addTarea();
+//         console.log('Tarea anadida: ' + miTareaAsync);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// function mostrarTarea() {
+//     console.log(' ');
+//     console.log(" Tareas: ");
+//     tasks.forEach((task, index) => {
+//         const statusTarea = task.completado ? 'Completada: ' : 'No Completada: ';
+//         console.log(`${index + 1}. ${statusTarea} ${task.addTareas}`);
+//         console.log(' ');
+//     });
+// }
+
+// function eliminarTarea() {
+//     return new Promise((resolve, reject) => {
+//         mostrarTarea();
+//         console.log(' ');
+//         const indiceTarea = readline.question('Que tarea quiere eliminar: ');
+//         console.log(' ');
+
+//         if (indiceTarea >= 0 && indiceTarea < tasks.length) {
+//             tasks.splice(indiceTarea, 1);
+//             resolve('Tarea Eliminada');
+//         } else {
+//             reject('Tarea no encontrada');
+//         }
+//     });
+// }
+
+// function completarTarea() {
+//     return new Promise((resolve, reject) => {
+//         mostrarTarea();
+//         console.log(' ');
+//         const indiceTarea = readline.questionInt('Que tarea quiere completar? ') - 1;
+//         console.log(' ');
+
+//         if (indiceTarea >= 0 && indiceTarea < tasks.length) {
+//             tasks[indiceTarea].completado = true;
+//             resolve('Tarea marcada como completa');
+//         } else {
+//             reject('Tarea no encontrada');
+//         }
+//     });
+// }
+
+// async function main() {
+//     while (true) {
+//         console.log('Opciones ');
+//         console.log(' ');
+//         console.log('1. Anadir Tarea ');
+//         console.log(' ');
+//         console.log('2. Mostrar Tareas ');
+//         console.log(' ');
+//         console.log('3. Eliminar Tarea ');
+//         console.log(' ');
+//         console.log('4. Completar Tareas ');
+//         console.log(' ');
+//         console.log('5. Salir ');
+//         console.log(' ');
+
+//         const opciones = readline.questionInt('Seleccione una Opcion: ');
+//         console.log(' ');
+
+//         switch (opciones) {
+//             case 1:
+//                 await addTareaAsync();
+//                 console.log('Tarea anadida exitosamente');
+//                 break;
+//             case 2:
+//                 mostrarTarea();
+//                 break;
+//             case 3:
+//                 try {
+//                     const message = await eliminarTarea();
+//                     console.log(message);
+//                 } catch (error) {
+//                     console.error('Error al eliminar tarea:', error);
+//                 }
+//                 break;
+//             case 4:
+//                 try {
+//                     const message = await completarTarea();
+//                     console.log(message);
+//                 } catch (error) {
+//                     console.error('Error al completar tarea:', error);
+//                 }
+//                 break;
+//             case 5:
+//                 return;
+//         }
+//     }
+// }
+
+// main();
+
+
+//Prueba con metodo 'then()'
 const readline = require('readline-sync');
 
 // Lista de tareas
@@ -86,27 +208,26 @@ async function main() {
 
         switch (opciones) {
             case 1:
-                await addTareaAsync();
-                console.log('Tarea anadida exitosamente');
+                addTareaAsync().then(()=> {
+                    console.log('Tarea anadida exitosamente');
+                });
                 break;
             case 2:
                 mostrarTarea();
                 break;
             case 3:
-                try {
-                    const message = await eliminarTarea();
+                eliminarTarea().then((message) => {
                     console.log(message);
-                } catch (error) {
+                }).catch((error) => {
                     console.error('Error al eliminar tarea:', error);
-                }
+                });
                 break;
             case 4:
-                try {
-                    const message = await completarTarea();
+                completarTarea().then((message) => {
                     console.log(message);
-                } catch (error) {
+                }).catch((error) => {
                     console.error('Error al completar tarea:', error);
-                }
+                });
                 break;
             case 5:
                 return;
